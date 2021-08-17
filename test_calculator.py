231e8685d9b74abe39f271953c8c14072c3fb18c
Cauverig <cauverig@gmail.com>
2021-08-17 14:17:24 -0700

    correcting errors

--- test_calculator.py
+++ test_calculator.py
@@ -8,10 +8,10 @@ import calculator
 
 class TestCalculator:
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
 
 
 if __name__ == '__main__':
