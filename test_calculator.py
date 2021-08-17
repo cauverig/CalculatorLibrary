79cf58fddef8040edb4205fef04d10923a07b684
Cauverig <cauverig@gmail.com>
2021-08-17 14:30:01 -0700

    added multiplication operation

--- test_calculator.py
+++ test_calculator.py
@@ -14,6 +14,9 @@ class TestCalculator:
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
 
 if __name__ == '__main__':
     unittest.main()
